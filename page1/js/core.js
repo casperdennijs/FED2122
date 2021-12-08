c077fb53618e1febb42b38bccb8fe5fda7cc9306
Casper <casper.dennijs@outlook.com>
2021-12-08 16:28:58 +0100

    Pagina 1 responsive design (~80% af)

---
+++ page1/js/core.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector("#menu")
+let button = document.querySelector("#button")
+let details = document.querySelectorAll("details")
+
+function openMenu() {
+    menu.classList.toggle("menuOpen")
+}
+
+button.addEventListener("click", openMenu)
+
+if (window.screen.width > 832) {
+    for (let i = 0; i < details.length; i++) {
+        details[i].open = true;
+    }
+} else {
+    for (let i = 0; i < details.length; i++) {
+        details[i].open = false;
+    }
+}
+console.log(window.screen.width)
